b9dffe01c9dd50eec22ca25537a459091922002c
GiftedR <kidsgamers@hotmail.com>
2025-09-24 13:06:03 -0700

    feat: Added Step 4

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ internal class Program
 {
 	private static void Main(string[] args)
 	{
-
+		int[] onekarray = new int[1000];
+		int[] tenkarray = new int[10000];
+		int[] hundredkarray = new int[100000];
 	}
 
 	private static int FirstElement(int[] arr) => arr.First();
