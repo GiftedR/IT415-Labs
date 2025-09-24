ad6c613bd8573f2fae46bf29017c4a5f8960ea9f
GiftedR <kidsgamers@hotmail.com>
2025-09-24 13:04:21 -0700

    feat: Completed Step 3

--- Program.cs
+++ Program.cs
@@ -6,4 +6,24 @@ internal class Program
 	{
 
 	}
+
+	private static int FirstElement(int[] arr) => arr.First();
+
+	private static int SumElements(int[] arr) => arr.Sum();
+
+	private static long CrossSearchElements(int[] arr)
+	{
+		long combos = 0;
+
+		for (int x = 0; x < arr.Length; x++)
+		{
+			for (int y = 0; y < arr.Length; y++)
+			{
+				combos++;
+				_ = $"{x}{y}";
+			}
+		}
+
+		return combos;
+	}
 }
