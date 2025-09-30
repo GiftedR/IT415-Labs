de41ad598f3567272cf53779de5a2b2ab7475e1d
GiftedR <kidsgamers@hotmail.com>
2025-09-30 13:07:03 -0700

    feat: Created Project

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+internal class Program
+{
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+	}
+}
