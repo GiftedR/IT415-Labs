ad4cc8988517b7cba3a39247559e7c0cfad4d450
GiftedR <kidsgamers@hotmail.com>
2025-10-05 15:03:06 -0700

    feat: Added Array demonstration

--- Program.cs
+++ Program.cs
@@ -6,6 +6,41 @@ internal class Program
 {
 	private static void Main(string[] args)
 	{
-		Console.WriteLine("Hello, World!");
+		RunArraySample();
+	}
+
+	public static void RunArraySample()
+	{
+		int[] studentIds = { 71892, 65198, 47384, 90873, 74893 };
+		Console.WriteLine($"1) Original array {string.Join(',', studentIds)}");
+
+		// Accessing by index
+		{
+			Console.WriteLine($"1A) Accessing by Index 3, value: {studentIds[3]}");
+		}
+
+		// Insert at middle by shifting
+		{
+			const int insertIndex = 2;
+			const int newStudentId = 57389;
+			Console.WriteLine($"1B) Insert new value {newStudentId} at index {insertIndex} by shifting.");
+			for (int idx = studentIds.Length - 1; idx > insertIndex; idx--)
+			{
+				studentIds[idx] = studentIds[idx - 1];
+			}
+			studentIds[insertIndex] = newStudentId;
+			Console.WriteLine($"1B) Adjusted array {string.Join(',', studentIds)}.");
+		}
+
+		// Delete at middle by shifting
+		{
+			const int deleteIndex = 1;
+			Console.WriteLine($"1C) Deleting value {studentIds[deleteIndex]} at index {deleteIndex} by shifting.");
+			for (int idx = deleteIndex; idx < studentIds.Length - 1; idx++)
+			{
+				studentIds[idx] = studentIds[idx + 1];
+			}
+			Console.WriteLine($"1C) Adjusted array {string.Join(',', studentIds)}.");
+		}
 	}
 }
