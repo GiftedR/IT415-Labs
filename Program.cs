c3e10133d6884f0acae1fc7c870c842d72396a37
GiftedR <kidsgamers@hotmail.com>
2025-10-05 16:31:08 -0700

    feat: Added string demonstration

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ internal class Program
 	private static void Main(string[] args)
 	{
 		RunArraySample();
+		RunStringSample();
 	}
 
 	public static void RunArraySample()
@@ -41,6 +42,54 @@ internal class Program
 				studentIds[idx] = studentIds[idx + 1];
 			}
 			Console.WriteLine($"1C) Adjusted array {string.Join(',', studentIds)}.");
+		}
+	}
+
+	public static void RunStringSample()
+	{
+		string students = "James Gunn";
+		const int newStudentsCount = 10000;
+		Stopwatch sw = new();
+		StringBuilder sb;
+
+		Console.WriteLine($"2) Original student string: {students}");
+		// Native Concatination
+		{
+			sw.Start();
+			for (int idx = 0; idx < newStudentsCount; idx++)
+			{
+				students += $", student{idx}";
+			}
+			sw.Stop();
+			Console.WriteLine($"2A) Added {newStudentsCount} student(s) using native concatination for a total time of: {sw.Elapsed}");
+		}
+
+		sb = new(students);
+
+		// Stringbuilder Concatination
+		{
+			sw.Restart();
+			for (int idx = newStudentsCount; idx < newStudentsCount * 2; idx++)
+			{
+				sb.Append($", student{idx}");
+			}
+			sw.Stop();
+			Console.WriteLine($"2B) Added {newStudentsCount} student(s) using stringbuilder for a total time of: {sw.Elapsed}");
+		}
+
+		//Formatting the string to be capital
+		{
+			string[] oldstrings = sb.ToString().Split(',');
+
+			for (int idx = 0; idx < oldstrings.Length; idx++)
+			{
+				if (oldstrings[idx].Length > 0)
+				{
+					oldstrings[idx] = char.ToUpper(oldstrings[idx].Trim()[0]) + oldstrings[idx].Trim().Substring(1);
+				}
+			}
+			sb = new(string.Join(", ", oldstrings));
+			Console.WriteLine($"2C) Capitalized student names: {sb.ToString(0, Math.Min(sb.Length, 100))}...");
 		}
 	}
 }
