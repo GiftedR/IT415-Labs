896e3119d9203f26b41165ded34ed38ee57d4ba2
GiftedR <kidsgamers@hotmail.com>
2025-10-15 09:56:25 -0700

    feat: Added method signitures

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿internal class Program
+{
+	private static void Main(string[] args)
+	{
+
+	}
+
+	static int Factorial(int n)
+	{
+
+	}
+
+	static int Fibonacci(int n)
+	{
+
+	}
+
+	static int SumArray(int[] arr, int index)
+	{
+
+	}
+	
+	static bool Contains(int[] arr, int index, int target)
+	{
+		
+	}
+}
