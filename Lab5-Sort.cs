fb2055426b6db0df901b73d49779ca5e43dbc50f
GiftedR <kidsgamers@hotmail.com>
2025-10-26 13:46:29 -0700

    feat: Changed quicksort to be iterative for stack overflow issues. Added XML comments

--- Lab5-Sort.cs
+++ Lab5-Sort.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 internal class Program
@@ -6,6 +7,7 @@ internal class Program
 	private static void Main(string[] args)
 	{
 		Stopwatch sw = new();
+		TimeSpan in10, in100, in1000, qs10, qs100, qs1000;
 
 		{ // Insertion Sorting
 			int[] insertarr = gen(10);
@@ -16,6 +18,7 @@ internal class Program
 			sw.Stop();
 			Console.Write("After:\n");
 			Console.Write($"\tInsertion Sort of 10 Items: {sw.Elapsed}\n\t");
+			in10 = sw.Elapsed;
 			print(insertarr);
 		}
 		{ // Insertion Sorting
@@ -27,6 +30,7 @@ internal class Program
 			sw.Stop();
 			Console.Write("After:\n");
 			Console.Write($"\tInsertion Sort of 100 Items: {sw.Elapsed}\n\t");
+			in100 = sw.Elapsed;
 			print(insertarr);
 		}
 		{ // Insertion Sorting
@@ -38,6 +42,7 @@ internal class Program
 			sw.Stop();
 			Console.Write("After:\n");
 			Console.Write($"\tInsertion Sort of 1000 Items: {sw.Elapsed}\n\t");
+			in1000 = sw.Elapsed;
 			print(insertarr);
 		}
 		{ // Quick Sorting
@@ -49,6 +54,7 @@ internal class Program
 			sw.Stop();
 			Console.Write("After:\n");
 			Console.Write($"\tQuick Sort of 10 Items: {sw.Elapsed}\n\t");
+			qs10 = sw.Elapsed;
 			print(quickarr);
 		}
 		{ // Quick Sorting
@@ -60,6 +66,7 @@ internal class Program
 			sw.Stop();
 			Console.Write("After:\n");
 			Console.Write($"\tQuick Sort of 100 Items: {sw.Elapsed}\n\t");
+			qs100 = sw.Elapsed;
 			print(quickarr);
 		}
 		{ // Quick Sorting
@@ -71,8 +78,13 @@ internal class Program
 			sw.Stop();
 			Console.Write("After:\n");
 			Console.Write($"\tQuick Sort of 1000 Items: {sw.Elapsed}\n\t");
+			qs1000 = sw.Elapsed;
 			print(quickarr);
 		}
+		Console.WriteLine("Results:");
+		Console.WriteLine("            |-------10---------|-------100--------|-------1000-------|");
+		Console.WriteLine($"Insert Sort | {in10} | {in100} | {in1000} |");
+		Console.WriteLine($"Quick Sort  | {qs10} | {qs100} | {qs1000} |");
 	}
 
 	private static void print(int[] arr)
@@ -91,6 +103,17 @@ internal class Program
 		return genarr;
 	}
 
+	/// <summary>
+	/// Sorts an array using the Insertion Algorithm.
+	/// </summary>
+	/// <param name="arr">The array to be sorted</param>
+	/// <remarks>
+	/// 	<complexity>
+	/// 		Best:    Ω(n)  - Best case is the array is already sorted.
+	/// 		Average: Θ(n²) - Average case increases operations by the amount of items each time.
+	/// 		Worst:   O(n²) - Worst case increases operations by the amount of items each time.
+	/// 	</complexity>
+	/// </remarks>
 	public static void InsertionSort(int[] arr)
 	{
 		int size = arr.Length;
@@ -109,7 +132,12 @@ internal class Program
 		}
 	}
 
-
+	/// <summary>
+	/// Used for quick sort, swaps to array elements.
+	/// </summary>
+	/// <param name="arr">The array being sorted</param>
+	/// <param name="i">Left swap index</param>
+	/// <param name="j">Right swap index</param>
 	private static void swap(int[] arr, int i, int j)
 	{
 		int tmp = arr[i];
@@ -117,6 +145,13 @@ internal class Program
 		arr[j] = tmp;
 	}
 
+	/// <summary>
+	/// Creates a partition for elements to be swapped.
+	/// </summary>
+	/// <param name="arr">The array affected</param>
+	/// <param name="low">Starting position of the pivot</param>
+	/// <param name="high">Ending position of the pivot</param>
+	/// <returns></returns>
 	private static int part(int[] arr, int low, int high)
 	{
 		int pivot = arr[high];
@@ -134,17 +169,43 @@ internal class Program
 		return i + 1;
 	}
 
-	public static void QuickSort(int[] arr, int low = 0, int high = -1)
+	/// <summary>
+	/// Sorts the input array.
+	/// </summary>
+	/// <param name="arr">The array to be sorted.</param>
+	/// <remarks>
+	/// 	Changed to iterative over recursive due to frequent stack overflows with larger arrays.
+	/// 	<complexity>
+	/// 		Best:    Ω(n log n) - Best case is the array is already sorted.
+	/// 		Average: Θ(n log n) - The sorting uses a splitting method, so increases only happen after a certain threshold.
+	/// 		Worst:   O(n²)      - Only occurs when the array is in a specific state.
+	/// 	</complexity>
+	/// </remarks>
+	public static void QuickSort(int[] arr)
 	{
-		if (high < 0)
-			high = arr.Length - 1;
+		int p, startidx = 0, endidx = arr.Length - 1, top = -1;
+		int[] stack = new int[arr.Length];
 
-		if (low < high)
+		stack[++top] = startidx;
+		stack[++top] = endidx;
+
+		while (top >= 0)
 		{
-			int partidx = part(arr, low, high);
+			endidx = stack[top--];
+			startidx = stack[top--];
 
-			QuickSort(arr, low, partidx - 1);
-			QuickSort(arr, partidx + 1, high);
+			p = part(arr, startidx, endidx);
+
+			if (p - 1 > startidx)
+			{
+				stack[++top] = startidx;
+				stack[++top] = p - 1;
+			}
+			if (p + 1 < endidx)
+			{
+				stack[++top] = p + 1;
+				stack[++top] = endidx;
+			}
 		}
 	}
 }
