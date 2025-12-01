5d8e09a1dfed190e266575a9074d78615370ed11
GiftedR <kidsgamers@hotmail.com>
2025-11-30 21:36:11 -0800

    bug: Added dfs history and checking against that history to avoid stack overflows

--- Bug10-Broken_DFS.cs
+++ Bug10-Broken_DFS.cs
@@ -1,4 +1,6 @@
-﻿internal class Program
+﻿using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
 	private static void Main(string[] args)
 	{
@@ -19,15 +21,20 @@
 		};
 
 		BrokenDFS("Carl", l1_Circ);
+		p_DFSHistory = new();
 		BrokenDFS("Carl", l2_Circ);
 	}
 
+	private static List<string> p_DFSHistory = new();
+
 	public static void BrokenDFS(string vertex, Dictionary<string, List<string>> graph)
 	{
+		p_DFSHistory.Add(vertex);
 		Console.WriteLine(vertex);
 		foreach (string neighbor in graph[vertex])
 		{
-			BrokenDFS(neighbor, graph);
+			if (!p_DFSHistory.Contains(neighbor))
+				BrokenDFS(neighbor, graph);
 		}
 	}
 }
