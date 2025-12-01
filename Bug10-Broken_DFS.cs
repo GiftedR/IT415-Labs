44bdf963a7fabbace7812d728a45a8dffe575f82
GiftedR <kidsgamers@hotmail.com>
2025-11-30 21:39:27 -0800

    docs: Added bug fix comments

--- Bug10-Broken_DFS.cs
+++ Bug10-Broken_DFS.cs
@@ -4,6 +4,7 @@ internal class Program
 {
 	private static void Main(string[] args)
 	{
+		// Test case one: Everything references every other thing
 		Dictionary<string, List<string>> l1_Circ = new()
 		{
 			{"Carl", [ "Bob", "Jeff", "Andy"]},
@@ -12,6 +13,7 @@ internal class Program
 			{"Andy", [ "Bob", "Jeff", "Carl"]}
 		};
 
+		// Test case two: Large circle of four nodes
 		Dictionary<string, List<string>> l2_Circ = new()
 		{
 			{"Carl", [ "Bob" ]},
@@ -21,19 +23,21 @@ internal class Program
 		};
 
 		BrokenDFS("Carl", l1_Circ);
-		p_DFSHistory = new();
+		p_DFSHistory = new(); // History reset needed in order to work properly and not change method signiture.
 		BrokenDFS("Carl", l2_Circ);
 	}
 
 	private static List<string> p_DFSHistory = new();
 
+	// Bug was in here, there was no check for duplicate parsing, now it instead bypasses the parsing if it is found within the history.
 	public static void BrokenDFS(string vertex, Dictionary<string, List<string>> graph)
 	{
+		// Adds item to the history
 		p_DFSHistory.Add(vertex);
 		Console.WriteLine(vertex);
 		foreach (string neighbor in graph[vertex])
 		{
-			if (!p_DFSHistory.Contains(neighbor))
+			if (!p_DFSHistory.Contains(neighbor)) // Only calls if not within history
 				BrokenDFS(neighbor, graph);
 		}
 	}
