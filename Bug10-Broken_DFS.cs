50b1e97a3641574b1bf455e95854c7b86048ec03
GiftedR <kidsgamers@hotmail.com>
2025-11-30 21:30:18 -0800

    feat: Added test data for breakage

--- Bug10-Broken_DFS.cs
+++ Bug10-Broken_DFS.cs
@@ -2,7 +2,24 @@
 {
 	private static void Main(string[] args)
 	{
-		
+		Dictionary<string, List<string>> l1_Circ = new()
+		{
+			{"Carl", [ "Bob", "Jeff", "Andy"]},
+			{"Bob", [ "Carl", "Jeff", "Andy"]},
+			{"Jeff", [ "Bob", "Carl", "Andy"]},
+			{"Andy", [ "Bob", "Jeff", "Carl"]}
+		};
+
+		Dictionary<string, List<string>> l2_Circ = new()
+		{
+			{"Carl", [ "Bob" ]},
+			{"Bob", [ "Jeff" ]},
+			{"Jeff", [ "Andy"]},
+			{"Andy", [ "Carl"]}
+		};
+
+		BrokenDFS("Carl", l1_Circ);
+		BrokenDFS("Carl", l2_Circ);
 	}
 
 	public static void BrokenDFS(string vertex, Dictionary<string, List<string>> graph)
